187ce8736913931f46cf22e1267b1c85a06d89b5
classjek <jekoniak13@gmail.com>
2026-02-16 17:50:21 -0800

    change is number of ground names considered

--- src/main.cpp
+++ src/main.cpp
@@ -127,8 +127,8 @@ std::vector<std::vector<std::vector<int>>> finalResults(universal_constraints.si
 
 // Build smaller set of groundNames for testing
 std::vector<std::vector<std::string>> groundNamesTest(typedGroundNames.size());
-groundNamesTest[0].assign(typedGroundNames[0].begin(), typedGroundNames[0].begin()+2); // genes 100
-groundNamesTest[1].assign(typedGroundNames[1].begin(), typedGroundNames[1].begin()+2); // enzymes 27
+groundNamesTest[0].assign(typedGroundNames[0].begin(), typedGroundNames[0].begin()+10); // genes 100
+groundNamesTest[1].assign(typedGroundNames[1].begin(), typedGroundNames[1].begin()+10); // enzymes 27
 groundNamesTest[2].assign(typedGroundNames[2].begin(), typedGroundNames[2].begin()+1); // reactions
 groundNamesTest[3].assign(typedGroundNames[3].begin(), typedGroundNames[3].begin()+1); //compounds
 
